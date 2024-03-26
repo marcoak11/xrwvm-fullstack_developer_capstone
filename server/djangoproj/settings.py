a6bd54e6824c4028d5145f7a5a6d36829728f255
marcoak11 <31699441+marcoak11@users.noreply.github.com>
2024-03-26 12:42:25 +0100

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,14 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','https://marquercia-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = ['https://marquercia-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS = ['localhost', 
+                 'https://marquercia-8000.'\
+                 'theiadockernext-0-labs-prod-theiak8s-4-tor01.'\
+                 'proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS = [
+    'https://marquercia-8000.' \
+    'theiadockernext-0-labs-prod-theiak8s-4-tor01.'\
+    'proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -94,7 +100,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation.'\
+        'UserAttributeSimilarityValidator',
     },
     {
         'NAME':
@@ -139,8 +146,7 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'frontend/static'),
+    os.path.join(BASE_DIR, 'frontend/static'),
     os.path.join(BASE_DIR, 'frontend/build'),
     os.path.join(BASE_DIR, 'frontend/build/static')
 ]
-
